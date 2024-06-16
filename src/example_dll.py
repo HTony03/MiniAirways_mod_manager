3c05ad53398c23221f05deeafacc521741ec669a
HTony03 <Tony20070322@163.com>
2024-06-16 16:53:09 +0800

    test dlls

--- src/example_dll.py
+++ src/example_dll.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':pass
 
 #from pythonnet import load
 #load("coreclr")
-import sys
+import sys,os
 sys.path.extend(r'.//ReferencedAssemblies')
 
 import pythonnet
@@ -15,3 +15,16 @@ print(module.GetName())
 print(type(module.GetName()))
 print(module.FullName)
 #help(clr.AddReference("VerticalLevel"))
+
+for filename in os.listdir(r'D:\Program Files (x86)\Steam\steamapps\common\Mini Airways Playtest\MiniAirways_mod_manager\src'):
+    if os.path.isfile(os.path.join(r'D:\Program Files (x86)\Steam\steamapps\common\Mini Airways Playtest\MiniAirways_mod_manager\src', filename)):
+
+        base, ext = os.path.splitext(filename)
+
+        while ext and base.count('.') > 0:
+            base, new_ext = os.path.splitext(base)
+            ext = new_ext + ext
+
+        if ext == '.dll':
+            module = clr.AddReference(base)
+            print(module.ToString().split(', ')[0])
